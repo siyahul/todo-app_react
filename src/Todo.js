ccfeeb698c0debc2f6660f45df7d7bf06c4f8a7d
Siyahul Haq <Siyahulhaq124@gmail.com>
2020-09-25 20:30:51 +0530

    Design Compleated

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
-import './Todo.css';
+import React, { Component } from "react";
+import "./Todo.css";
 
 export default class Todo extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="todo">
+        <form className="todo__inputSection">
+          <h1>Todo App</h1>
+          <input
+            placeholder="Enter items"
+            className="todo__inputSection__input"
+          ></input>
+        </form>
+        <ul>
+          <li>
+            Items <i className="fas fa-trash-alt"></i>
+          </li>
+          <li>
+            Items <i className="fas fa-trash-alt"></i>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
