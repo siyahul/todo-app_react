0664af5ef0bc24bb9f11570ebad545fca8ec65b8
Siyahul Haq <Siyahulhaq124@gmail.com>
2020-09-26 00:10:47 +0530

    add and remove actions added

--- src/Todo.js
+++ src/Todo.js
@@ -2,23 +2,56 @@ import React, { Component } from "react";
 import "./Todo.css";
 
 export default class Todo extends Component {
+  state = {
+    text: "",
+    items: [],
+  };
+  handleChange = (event) => {
+    this.setState({
+      input: event.target.value,
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { input } = this.state;
+    const allItems = this.state.items;
+    allItems.push(input);
+    this.setState({
+      items: allItems,
+      input: "",
+    });
+  };
+  remove(id) {
+    const allItems = this.state.items;
+    allItems.splice(id, 1);
+    this.setState({
+      items: allItems,
+    });
+  }
   render() {
+    const { input, items } = this.state;
+    console.log(this.state.items);
     return (
       <div className="todo">
-        <form className="todo__inputSection">
+        <form className="todo__inputSection" onSubmit={this.handleSubmit}>
           <h1>Todo App</h1>
           <input
             placeholder="Enter items"
             className="todo__inputSection__input"
+            value={input}
+            onChange={this.handleChange}
           ></input>
         </form>
         <ul>
-          <li>
-            Items <i className="fas fa-trash-alt"></i>
-          </li>
-          <li>
-            Items <i className="fas fa-trash-alt"></i>
-          </li>
+          {items.map((item, id) => (
+            <li>
+              {item}{" "}
+              <i
+                onClick={() => this.remove(id)}
+                className="fas fa-trash-alt"
+              ></i>
+            </li>
+          ))}
         </ul>
       </div>
     );
